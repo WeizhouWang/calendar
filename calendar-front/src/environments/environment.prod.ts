53c52b1c631b68386ad7eeccee1ffe823239fee0
Weizhou Wamg <david.wangwzh@gmail.com>
2025-07-09 22:28:42 +0800

    change uri

--- calendar-front/src/environments/environment.prod.ts
+++ calendar-front/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  applicationURI: 'https://ecalendar.azurewebsites.net',
+  applicationURI: 'http://calendarweb-dev.eba-tqax9gue.us-east-1.elasticbeanstalk.com',
 };
